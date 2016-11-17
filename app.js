c2def2bf1455e2feba76946a7403b7a3f517eff4
susuaung <susuaung.ms@gmail.com>
2016-11-17 11:44:12 +0900

    add view engine and define static path

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use('/assets/', app.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
     res.send('Hello');
 });
