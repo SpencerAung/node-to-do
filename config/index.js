2cce7fcdf6ecb5089f1566fd1cad58d0405c7fc5
susuaung <susuaung.ms@gmail.com>
2016-11-18 10:55:16 +0900

    fix mongodb connection string

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ var config = require('./config.json');
 
 module.exports = {
     getDbConnectionString: function(){
-        return `mongodb: ${ config.uname }:${ config.pwd }@localhost:27017/nodetodo`;
+        return `mongodb://${ config.uname }:${ config.pwd }@localhost:27017/nodetodo`;
     }
 }
