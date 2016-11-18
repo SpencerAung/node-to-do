a2d4d9b57b8ad22cf3c5256d5e958d6263847f84
susuaung <susuaung.ms@gmail.com>
2016-11-18 10:21:44 +0900

    setup config and model for mongodb

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var app = express();
-
+var mongoose = require('mongoose');
+var config = require('./config');
 
 var port = process.env.PORT || 3000;
+
+
+
 
 app.use('/assets/', app.static(__dirname + '/public'));
 
@@ -12,4 +16,5 @@ app.get('/', function(req, res){
     res.send('Hello');
 });
 
+mongoose.connect(config.getDbConnectionString());
 app.listen(port);
---
+++ config/index.js
@@ -0,0 +1,7 @@
+var config = require('./config.json');
+
+module.exports = {
+    getDbConnectionString: function(){
+        return `mongodb: ${ config.uname }:${ config.pwd }@localhost:27017/nodetodo`;
+    }
+}
