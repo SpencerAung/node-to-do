63bf7dc5bab5be00c18256f12cd9a77e31b4bc33
susuaung <susuaung.ms@gmail.com>
2016-11-18 10:50:49 +0900

    fix error

--- app.js
+++ app.js
@@ -5,14 +5,8 @@ var config = require('./config');
 
 var port = process.env.PORT || 3000;
 
-<<<<<<< HEAD
-
-
-
-app.use('/assets/', app.static(__dirname + '/public'));
-=======
 app.use('/assets/', express.static(__dirname + '/public'));
->>>>>>> initial-setup
+
 
 app.set('view engine', 'ejs');
 
